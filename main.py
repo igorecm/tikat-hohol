55c28fd0386283e6feb2cb76358794a5f231c60f
igorecm <66513964+igorecm@users.noreply.github.com>
2022-03-09 15:14:53 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+﻿import pygame
 from pygame.locals import *
 import sys
 
@@ -16,12 +16,21 @@ tile_size = 32
 fps_clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption('Тыкать хохлика v1.0 <3')
+pygame.display.set_caption('Резать Любу v1.0 хихихиха эдишн')
 icon = pygame.image.load('res/icon.png')
 pygame.display.set_icon(icon)
 
 run = True
 score = 0
+def blud(x,y, vis):
+    img = pygame.image.load('res/blood.png')
+    img = pygame.transform.scale(img, (256, 256))
+    if vis == True:
+        img.set_alpha(0)
+        pass
+    elif vis == False:
+        img.set_alpha(255)
+    screen.blit(img, (x,y))
     
 class Hand():
     def __init__(self, x, y):
@@ -37,8 +46,10 @@ class Hand():
         # draw
         screen.blit(self.image, self.rect)
 
-        if self.rect.y > -120:
-            dy -= 10
+        if self.rect.y > -170:
+            dy -= 30
+            blud(280, 140, False)
+            
 
         # pos update
         self.rect.x += dx
@@ -48,18 +59,22 @@ def hohol(x,y):
     img = pygame.image.load('res/hohol.png')
     img = pygame.transform.scale(img, (320, 200))
     screen.blit(img, (x,y))
+
     
 hand = Hand (320, -120)
 while run:
     screen.fill((255, 255, 255))
-    hohol(240, 250)
     hand.update()
     
-    textsurface1 = myfont.render('тыкать хохлика - ЛКМ', False, (0, 0, 0))
+    hohol(240, 250)
+    
+    textsurface1 = myfont.render('резать любу - ЛКМ', False, (0, 0, 0))
     screen.blit(textsurface1,(0,0))
     
-    textsurface2 = myfont.render('тыкнуто раз: {0}'.format(score), False, (0, 0, 0))
+    textsurface2 = myfont.render('урон: {0}'.format(score), False, (0, 0, 0))
     screen.blit(textsurface2,(0,430))
+
+    
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,7 +82,7 @@ while run:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_presses = pygame.mouse.get_pressed()
             if mouse_presses[0]:
-                hand = Hand (320, -70)
+                hand = Hand (320, 0)
                 score += 1
                 sound1.play()
 
